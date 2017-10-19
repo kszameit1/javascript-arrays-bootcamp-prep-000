075e006c3506c4c0c8064c166ac8202a39519ae6
Karen Lowe <k.szameit@mac.com>
2017-10-19 11:24:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,14 +4,14 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
 
-function destructivelyAddElementToBeginningOfArray([array], element) {
-  return [array].unshift(element)
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element)
 }
 
 function addElementToEndOfArray(array, element) {
   return [...array, element]
 }
 
-function destructivelyAddElementToEndOfArray([array], element) {
-  return [array].push(element)
+function destructivelyAddElementToEndOfArray(array, element) {
+  return array.push(element)
 }
