c220da996904bb81fe81792a151c2b9a0590eab2
Karen Lowe <k.szameit@mac.com>
2017-10-19 11:47:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,14 +4,14 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  return [array.unshift(element)]
+function destructivelyAddElementToBeginningOfArray([], element) {
+  return array.unshift(element)
 }
 
 function addElementToEndOfArray(array, element) {
   return [...array, element]
 }
 
-function destructivelyAddElementToEndOfArray(array, element) {
-  return [array.push(element)]
+function destructivelyAddElementToEndOfArray([], element) {
+  return array.push(element)
 }
